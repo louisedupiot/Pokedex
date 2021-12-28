118bde950e058aee750a1124ff9df136af529666
Louise Dupiot <dupiot.louise@gmail.com>
2021-12-28 17:28:17 +0100

    details

--- src/components/PokemonsCartes.jsx
+++ src/components/PokemonsCartes.jsx
@@ -12,9 +12,9 @@ const PokemonsCartes = ({id, name, image, type, type2, weight, height, abilities
             <div className='details'>
                 <h3>{name}</h3>
                 
-                <small className='details'> <b>Poids</b> {weight}</small>
-                <small className='details'><b>Taille</b> {height}</small>
-                <small className='details'><b>Type</b> {type.map((index)=> <li key={id++}>{index.type.name}</li>)}</small>
+                <small className='details'> <b>Poids</b> {weight} lbs </small>
+                <small className='details'><b>Taille</b> {height}"</small>
+                <small className='details'><b>Type</b> {type.map((index)=> <li key={id++}>{index.type.name}</li>)}</small> 
                 <small className='details'><b>Capacités</b> {abilities.map((index)=> <li key={id++}>{index.ability.name}</li>)}</small>
                
             </div>
